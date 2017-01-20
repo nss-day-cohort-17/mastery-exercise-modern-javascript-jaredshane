e5c6799c414b80dc5febd66c804fac9b3ee9f8c8
Jared Fuller <jaredshane@gmail.com>
2017-01-20 10:23:29 -0600

    added an if statement to check if both player names are filled

--- quiz.js
+++ quiz.js
@@ -7,9 +7,11 @@ $('.saveNameButton').click(function (e) {
   player2 = $('.player2Input')[0].value;
   console.log($('.player1Input')[0].value)
   console.log($('.player2Input')[0].value);
-  if (player1 == "") {
-    alert('whoaaaa enter a name for player 1, bud')
+  if (player1 == "" && player2 == "") {
+    alert('whoaaaa enter a name for both players, bud')
+  } else if (player1 == "") {
+    alert('hey partner, enter a name for player 1')
   } else if (player2 == "") {
-    alert('hey partner, enter a name for player 2')
+    alert('hey buddy, I need that name for player 2')
   }
 });
